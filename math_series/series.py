0c66d9c153578e583949c0813d9d4f526efb0b5a
Saraaltaweel <s.bata97.st@gmail.com>
2021-05-03 22:13:31 +0300

    lab2

--- math_series/series.py
+++ math_series/series.py
@@ -1,19 +1,18 @@
 def fibonacci(n):
     
-    if n==0:
-        return n
-    if n==1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+  return  sum_series(n)
+
 
 def lucas(n):
     
-    if n==0:
-        return n
-    if n==1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+    return  sum_series(n,2,1)
+
         
- 
+def sum_series(n,num1=0,num2=1):
+    if n>=0:
+        if n==0:
+          return str(n)
+        if n==1:
+          return str(n)
+        else:
+          return str(sum_series(n-1,num1,num2) + sum_series(n-2,num1,num2))
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,21 +1,34 @@
 
-from math-series import __version__
-from math-series.series import fibonacci
+from math_series.series import fibonacci,lucas,sum_series
 
 
-def fibonacci-one():
-    actual = fibonacci(1)
+def fibonacci_one():
+    actual = fibonacci(0)
     expected = "0"
     assert actual == expected
 
-def fibonacci-two():
-    actual = fibonacci(2)
+def fibonacci_two():
+    actual = fibonacci(1)
     expected = "1"
     assert actual == expected
 
-def fibonacci-three():
-     actual = fibonacci(0)+fibonacci(1)
-    expected = "1"
+def fibonacci_three():
+    actual = fibonacci(5)
+    expected = "5"
     assert actual == expected
 
  
+def lucas_one():
+    actual = lucas(0)
+    expected = "2"
+    assert actual == expected
+
+def lucas_two():
+    actual = lucas(1)
+    expected = "1"
+    assert actual == expected
+
+def lucas_three():
+    actual = lucas(5)
+    expected = "11"
+    assert actual == expected
